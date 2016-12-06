5231adf1b8913df09ef6b5b4c4e77163a886e671
Elisabeth Hamel <elisabeth@stereosuper.fr>
2016-12-06 16:11:04 +0100

    :art: category

---
+++ dest/wp-content/themes/thinkovery/includes/posts.php
@@ -0,0 +1,52 @@
+<?php $formatLink = get_post_format() === 'link' ? true : false; ?>
+
+<div class='post <?php if($formatLink) echo "post-network"; ?>'>
+
+    <?php if(!$formatLink){ ?>
+
+        <h2 class='h3'><a href='<?php the_permalink(); ?>'><?php the_title(); ?></a></h2>
+        <time datetime='<?php echo get_the_date('Y-m-d'); ?>'><?php echo get_the_date(); ?></time>
+        <span>
+            <?php $cats = get_the_category(); if($cats){
+                $countCats = count($cats);
+                $i = 0;
+                foreach($cats as $cat){
+                    $i ++;
+                    echo $cat->cat_name;
+                    if($i < $countCats){
+                        echo ', ';
+                    }
+                }
+            } ?>
+        </span>
+        <?php $nbComments = get_comments_number(); ?>
+
+        <?php if($nbComments > 0){ ?>
+            <a href='<?php the_permalink(); ?>#comments'>
+                <?php echo sprintf( _n('%s comment', '%s comments', $nbComments, 'thinkovery'), number_format_i18n( $nbComments ) ); ?>
+            </a>
+        <?php }else{ ?>
+                <a href='<?php the_permalink(); ?>#leave-comment'><?php _e('Leave a comment', 'thinkovery'); ?>
+        <?php } ?>
+
+        <a href='<?php the_permalink(); ?>'>
+            <?php if( has_post_thumbnail() ){ the_post_thumbnail(); } ?>
+        </a>
+
+        <?php the_excerpt(); ?>
+
+    <?php }else{ ?>
+
+        <a href='<?php the_field('link'); ?>' target='_blank'>
+            <?php if( has_post_thumbnail() ){ the_post_thumbnail(); } ?>
+        </a>
+
+        <?php the_excerpt(); ?>
+
+        <a href='<?php the_field('link'); ?>' target='_blank'>
+            <?php echo __('Find us on', 'thinkovery') . ' ' . get_field('network'); ?>
+        </a>
+
+    <?php } ?>
+
+</div>
