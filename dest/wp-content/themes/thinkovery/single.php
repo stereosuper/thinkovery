7ffe551b0b4618db0052ddba232b77e48ef88e62
Elisabeth Hamel <elisabeth@stereosuper.fr>
2016-12-07 18:40:18 +0100

    :art: single thumbnail

--- dest/wp-content/themes/thinkovery/single.php
+++ dest/wp-content/themes/thinkovery/single.php
@@ -6,6 +6,8 @@
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
+				<?php the_post_thumbnail(); ?>
+
 				<h1><?php the_title(); ?></h1>
 				<time datetime='<?php echo get_the_date('Y-m-d'); ?>'><?php echo get_the_date(); ?></time>
 
