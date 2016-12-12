01d0aa998218f9e3e7ed24c426eae1e628f501c6
Elisabeth Hamel <elisabeth@stereosuper.fr>
2016-12-12 12:32:03 +0100

    :art: scroll indicator

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,18 @@
 'use strict';
 
 var $ = require('./libs/jquery/dist/jquery.slim.min.js');
+var TweenMax = require('./libs/gsap/src/uncompressed/TweenMax.js');
 
 $(function(){
 
     window.requestAnimFrame = require('./requestAnimFrame.js');
     var throttle = require('./throttle.js');
 
+    var windowWidth = $(window).outerWidth(), windowHeight = $(window).height(), docHeight = $(document).height();
+    var myScroll, scrollPercent, scrollProgress;
+
     var body = $('body');
-    var windowWidth = $(window).outerWidth(), windowHeight = $(window).height();
+    var logo = $('#logo'), logoO = logo.find('#logo-o'), logoO1 = logo.find('#logo-o1');
 
 
 
@@ -19,6 +23,7 @@ $(function(){
 
         windowWidth = $(window).outerWidth();
         windowHeight = $(window).height();
+        docHeight = $(document).height();
 
 	}, 60)).on('load', function(){
 
@@ -27,6 +32,25 @@ $(function(){
 
     $(document).on('scroll', throttle(function(){
 
-    }, 60));
+        myScroll = $(document).scrollTop();
+        scrollPercent = (myScroll-10) / (docHeight-windowHeight);
+        scrollProgress = scrollPercent*353;
+
+        if(myScroll > 10){
+            if(!logo.hasClass('on')){
+                logo.addClass('on');
+                TweenMax.to(logoO, 0.3, {opacity: 1});
+                TweenMax.set(logoO1, {opacity: 0});
+            }
+            TweenMax.set(logoO, {x: scrollProgress + 'px'});
+        }else{
+            if(logo.hasClass('on')){
+                logo.removeClass('on');
+                TweenMax.to(logoO, 0.3, {opacity: 0});
+                TweenMax.set(logoO1, {opacity: 1});
+            }
+        }
+
+    }, 10));
 
 });
