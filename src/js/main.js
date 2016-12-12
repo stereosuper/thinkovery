f1c6401f2e86510688ba54ae327d40585c35c222
Elisabeth Hamel <elisabeth@stereosuper.fr>
2016-12-12 14:15:11 +0100

    :art: hover header

--- src/js/main.js
+++ src/js/main.js
@@ -11,7 +11,7 @@ $(function(){
     var windowWidth = $(window).outerWidth(), windowHeight = $(window).height(), docHeight = $(document).height();
     var myScroll, scrollPercent, scrollProgress;
 
-    var body = $('body');
+    var body = $('body'), header = $('#header');
     var logo = $('#logo'), logoO = logo.find('#logo-o'), logoO1 = logo.find('#logo-o1');
 
 
@@ -19,15 +19,26 @@ $(function(){
     // isMobile.any ? body.addClass('is-mobile') : body.addClass('is-desktop');
 
 
-    $(window).on('resize', throttle(function(){
+    function showScrollIndic(){
+        if(!logo.hasClass('on')){
+            logo.addClass('on');
+            TweenMax.to(logoO, 0.3, {opacity: 1});
+            TweenMax.set(logoO1, {opacity: 0});
+        }
+    }
+    function hideScrollIndic(){
+        if(logo.hasClass('on')){
+            logo.removeClass('on');
+            TweenMax.to(logoO, 0.3, {opacity: 0});
+            TweenMax.set(logoO1, {opacity: 1});
+        }
+    }
 
-        windowWidth = $(window).outerWidth();
-        windowHeight = $(window).height();
-        docHeight = $(document).height();
-
-	}, 60)).on('load', function(){
-
-	});
+    header.on('mouseenter', hideScrollIndic).on('mouseleave', function(){
+        if(myScroll > 10){
+            showScrollIndic();
+        }
+    });
 
 
     $(document).on('scroll', throttle(function(){
@@ -37,20 +48,21 @@ $(function(){
         scrollProgress = scrollPercent*353;
 
         if(myScroll > 10){
-            if(!logo.hasClass('on')){
-                logo.addClass('on');
-                TweenMax.to(logoO, 0.3, {opacity: 1});
-                TweenMax.set(logoO1, {opacity: 0});
-            }
+            showScrollIndic();
             TweenMax.set(logoO, {x: scrollProgress + 'px'});
         }else{
-            if(logo.hasClass('on')){
-                logo.removeClass('on');
-                TweenMax.to(logoO, 0.3, {opacity: 0});
-                TweenMax.set(logoO1, {opacity: 1});
-            }
+            hideScrollIndic();
         }
 
     }, 10));
+
+
+    $(window).on('resize', throttle(function(){
+
+        windowWidth = $(window).outerWidth();
+        windowHeight = $(window).height();
+        docHeight = $(document).height();
+
+    }, 60));
 
 });
