0c11c9e3c0bbcbb263809fdc8573375337c16b40
Elisabeth Hamel <elisabeth@stereosuper.fr>
2016-12-13 18:55:24 +0100

    :art: install wp-rocket

--- dest/wp-config.php
+++ dest/wp-config.php
@@ -1,4 +1,5 @@
 <?php
+define('WP_CACHE', true); // Added by WP Rocket
 # BEGIN SecuPress Correct Constants Values
 define( 'DISALLOW_UNFILTERED_HTML', TRUE ); // Added by SecuPress
 define( 'ERRORLOGFILE', '/Applications/MAMP/logs/wp_errorlogfile.log' ); // Added by SecuPress
