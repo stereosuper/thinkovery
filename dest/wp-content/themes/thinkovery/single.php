93dec4da25e884340d3d6f9827049852614d9fdb
Elisabeth Hamel <elisabeth@stereosuper.fr>
2016-12-19 15:41:55 +0100

    :art: update single

--- dest/wp-content/themes/thinkovery/single.php
+++ dest/wp-content/themes/thinkovery/single.php
@@ -2,96 +2,92 @@
 
 	<article class='container'>
 
-		<?php if ( have_posts() ) : ?>
+		<div class='container-small'>
 
-			<?php while ( have_posts() ) : the_post(); ?>
+			<?php if ( have_posts() ) : ?>
 
-				<?php the_post_thumbnail(); ?>
+				<?php while ( have_posts() ) : the_post(); ?>
 
-				<h1><?php the_title(); ?></h1>
-				<time datetime='<?php echo get_the_date('Y-m-d'); ?>'><?php echo get_the_date(); ?></time>
+					<?php the_post_thumbnail(); ?>
 
-				<?php if(get_field('intro')){ ?>
-				    <p class='intro'><?php the_field('intro'); ?></p>
-				<?php } ?>
+					<time datetime='<?php echo get_the_date('Y-m-d'); ?>'><?php echo get_the_date(); ?></time>
+					<h1><?php the_title(); ?></h1>
 
-				<?php the_content(); ?>
+					<?php if(get_field('intro')){ ?>
+					    <p class='intro'><?php the_field('intro'); ?></p>
+					<?php } ?>
 
-				<div>
-					<p><?php _e('Share on', 'thinkovery'); ?></p>
-					<ul>
-						<li>
-							<a href='http://twitter.com/share?url=<?php the_permalink(); ?>&text=<?php the_title(); ?>&via=<?php bloginfo("name"); ?>' rel='nofollow' target='_blank'><?php _e('Share on Twitter', 'thinkovery'); ?></a>
-						</li>
-						<li>
-							<a href='http://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink(); ?>&title=<?php the_title(); ?>' rel='nofollow' target='_blank'><?php _e('Share on Linkedin', 'thinkovery'); ?></a>
-						</li>
-						<li>
-							<a href='http://www.facebook.com/sharer.php?u=<?php the_permalink(); ?>&t=<?php the_title(); ?>' rel='nofollow' target='blank'><?php _e('Share on Facebook', 'thinkovery'); ?></a>
-						</li>
-					</ul>
-				</div>
+					<?php the_content(); ?>
 
-				<?php
-					$currentPost = $post;
-					global $post;
+					<div>
+						<p><?php _e('Share this post', 'thinkovery'); ?></p>
+						<ul>
+							<li>
+								<a href='http://twitter.com/share?url=<?php the_permalink(); ?>&text=<?php the_title(); ?>&via=<?php bloginfo("name"); ?>' rel='nofollow' target='_blank'><?php _e('Share on Twitter', 'thinkovery'); ?></a>
+							</li>
+							<li>
+								<a href='http://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink(); ?>&title=<?php the_title(); ?>' rel='nofollow' target='_blank'><?php _e('Share on Linkedin', 'thinkovery'); ?></a>
+							</li>
+							<li>
+								<a href='http://www.facebook.com/sharer.php?u=<?php the_permalink(); ?>&t=<?php the_title(); ?>' rel='nofollow' target='blank'><?php _e('Share on Facebook', 'thinkovery'); ?></a>
+							</li>
+						</ul>
+					</div>
 
-					$categories = get_the_category($post->ID);
-					if($categories){
-						$categoryIds = array();
-						foreach($categories as $cat){
-							$categoryIds[] = $cat->term_id;
+					<?php
+						$currentPost = $post;
+						global $post;
+
+						$categories = get_the_category($post->ID);
+						if($categories){
+							$categoryIds = array();
+							foreach($categories as $cat){
+								$categoryIds[] = $cat->term_id;
+							}
+
+							$relatedQuery = new WP_Query( array(
+								'category__in' => $categoryIds,
+								'post__not_in' => array($post->ID),
+								'posts_per_page'=> 2,
+								'caller_get_posts'=>1,
+						        'tax_query' => array( array(
+						            'taxonomy' => 'post_format',
+						            'field' => 'slug',
+						            'terms' => array('post-format-link'),
+						            'operator' => 'NOT IN'
+						        ) )
+							) );
+
+							if( $relatedQuery->have_posts() ){
+								echo '<div><p>' . __('Related Posts', 'thinkovery') . '</p><ul>';
+								while( $relatedQuery->have_posts() ){ $relatedQuery->the_post(); ?>
+									<li>
+										<a href='<?php the_permalink(); ?>'>
+											<?php the_post_thumbnail(); ?>
+											<?php the_title(); ?>
+										</a>
+									</li>
+								<? }
+								echo '</ul></div>';
+							}
 						}
 
-						$relatedQuery = new WP_Query( array(
-							'category__in' => $categoryIds,
-							'post__not_in' => array($post->ID),
-							'posts_per_page'=> 2,
-							'caller_get_posts'=>1
-						) );
+						$post = $currentPost;
+						wp_reset_query();
+					?>
 
-						if( $relatedQuery->have_posts() ){
-							echo '<div><h3>' . __('Related Posts', 'thinkovery') . '</h3><ul>';
-							while( $relatedQuery->have_posts() ){ $relatedQuery->the_post(); ?>
-								<li>
-									<a href='<?php the_permalink(); ?>'>
-										<?php the_post_thumbnail(); ?>
-										<h3><?php the_title(); ?></h3>
-										<time datetime='<?php echo get_the_date('Y-m-d'); ?>'><?php echo get_the_date(); ?></time>
-										<span>
-											<?php $cats = get_the_category(); if($cats){
-												$countCats = count($cats);
-												$i = 0;
-												foreach($cats as $cat){
-													$i ++;
-													echo $cat->cat_name;
-													if($i < $countCats){
-														echo ', ';
-													}
-												}
-											} ?>
-										</span>
-									</a>
-								</li>
-							<? }
-							echo '</ul></div>';
-						}
-					}
+					<?php comments_template(); ?>
 
-					$post = $currentPost;
-					wp_reset_query();
-				?>
-
-				<?php comments_template(); ?>
-
-			<?php endwhile; ?>
+				<?php endwhile; ?>
 
 
-		<?php else : ?>
+			<?php else : ?>
 
-			<h1>404</h1>
+				<h1>404</h1>
 
-		<?php endif; ?>
+			<?php endif; ?>
+
+		</div>
 
 	</article>
 
