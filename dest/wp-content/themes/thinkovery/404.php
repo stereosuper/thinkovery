1605f1292a1a99ef3755ba9baa858ccf10f8a01a
Elisabeth Hamel <elisabeth@stereosuper.fr>
2016-12-21 15:16:03 +0100

    :art: 404

--- dest/wp-content/themes/thinkovery/404.php
+++ dest/wp-content/themes/thinkovery/404.php
@@ -1,6 +1,8 @@
 <?php get_header(); ?>
 
-	<h1>404</h1>
-	<p>Désolée les gars!</p>
+    <img src='<?php echo get_template_directory_uri(); ?>/layoutImg/404.jpg' alt="<?php _e("Erreur 404 - Trump n'a pas du regarder de MOOC..", 'thinkovery'); ?>">
+    <p class='h2'>
+        <?php _e("Lui, il n'a pas du regarder de", 'thinkovery'); ?> <span>M<span>O</span>OC</span>...
+    </p>
 
 <?php get_footer(); ?>
