f7616776fd0512123da50cfec2304b6305285a05
Elisabeth Hamel <elisabeth@stereosuper.fr>
2016-12-21 19:13:58 +0100

    :art: about

--- src/theme/about.php
+++ src/theme/about.php
@@ -6,26 +6,49 @@ Template Name: About
 get_header(); ?>
 
 	<div class='container'>
-		<div class='container-small'>
-	        <?php if ( have_posts() ) : the_post(); ?>
 
-	            <h1><?php the_title(); ?></h1>
+		<?php if ( have_posts() ) : the_post(); ?>
 
-	            <h2><?php the_field('title1'); ?></h2>
-	            <?php the_field('txt1'); ?>
+		    <div class='container-small'><h1><?php the_title(); ?></h1></div>
 
-	            <h2><?php the_field('title2'); ?></h2>
-	            <?php the_field('txt2'); ?>
+		    <div class='about-img about-img1'>
+		    	<img src='<?php echo get_template_directory_uri(); ?>/layoutImg/about1.jpg' alt='Thinkovery team'>
+		    	<svg class='icon hoop' style='fill:url(<?php echo $currentUrl; ?>#gradient-hoop')'><use xlink:href='#icon-hoop-thin'/></svg>
+		    	<div class='mask'></div>
+		    </div><div class='about-txt col-4'>
+		    	<h2><?php the_field('title1'); ?></h2>
+		    	<?php the_field('txt1'); ?>
+		    </div>
 
-	            <h2><?php the_field('title3'); ?></h2>
-	            <?php the_field('txt3'); ?>
+		    <div class='about-img about-img2'>
+		    	<img src='<?php echo get_template_directory_uri(); ?>/layoutImg/about2.jpg' alt=''>
+		    </div><div class='about-img about-img3'>
+		    	<img src='<?php echo get_template_directory_uri(); ?>/layoutImg/about3.jpg' alt=''>
+		    	<svg class='icon hoop' style='fill:url(<?php echo $currentUrl; ?>#gradient-hoop')'><use xlink:href='#icon-hoop'/></svg>
+		    	<div class='mask'></div>
+		    </div>
 
-	        <?php else : ?>
+		    <div class='about-txt col-4'>
+		    	<h2><?php the_field('title2'); ?></h2>
+		    	<?php the_field('txt2'); ?>
+		    </div><div class='about-img about-img4'>
+		    	<img src='<?php echo get_template_directory_uri(); ?>/layoutImg/about4.jpg' alt=''>
+		    </div>
 
-	            <h1>404</h1>
+		    <div class='about-img about-img5'>
+		    	<img src='<?php echo get_template_directory_uri(); ?>/layoutImg/about5.jpg' alt=''>
+		    	<svg class='icon hoop' style='fill:url(<?php echo $currentUrl; ?>#gradient-hoop')'><use xlink:href='#icon-hoop'/></svg>
+		    </div><div class='about-txt col-3'>
+		    	<h2><?php the_field('title3'); ?></h2>
+		    	<?php the_field('txt3'); ?>
+		    </div>
 
-	        <?php endif; ?>
-        </div>
+		<?php else : ?>
+
+		    <h1>404</h1>
+
+		<?php endif; ?>
+
     </div>
 
 <?php get_footer(); ?>
