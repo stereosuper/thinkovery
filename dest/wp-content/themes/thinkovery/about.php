b81d54c6ae3fe86bf8845a7e89d518881ea840f6
Elisabeth Hamel <elisabeth@stereosuper.fr>
2016-12-22 11:17:20 +0100

    :art: about

--- dest/wp-content/themes/thinkovery/about.php
+++ dest/wp-content/themes/thinkovery/about.php
@@ -15,7 +15,7 @@ get_header(); ?>
 		    	<img src='<?php echo get_template_directory_uri(); ?>/layoutImg/about1.jpg' alt='Thinkovery team'>
 		    	<svg class='icon hoop' style='fill:url(<?php echo $currentUrl; ?>#gradient-hoop')'><use xlink:href='#icon-hoop-thin'/></svg>
 		    	<div class='mask'></div>
-		    </div><div class='about-txt col-4'>
+		    </div><div class='about-txt about-txt1 col-4'>
 		    	<h2><?php the_field('title1'); ?></h2>
 		    	<?php the_field('txt1'); ?>
 		    </div>
@@ -28,19 +28,29 @@ get_header(); ?>
 		    	<div class='mask'></div>
 		    </div>
 
-		    <div class='about-txt col-4'>
+		    <div class='about-txt about-txt2 col-4'>
 		    	<h2><?php the_field('title2'); ?></h2>
 		    	<?php the_field('txt2'); ?>
 		    </div><div class='about-img about-img4'>
 		    	<img src='<?php echo get_template_directory_uri(); ?>/layoutImg/about4.jpg' alt=''>
-		    </div>
-
-		    <div class='about-img about-img5'>
-		    	<img src='<?php echo get_template_directory_uri(); ?>/layoutImg/about5.jpg' alt=''>
-		    	<svg class='icon hoop' style='fill:url(<?php echo $currentUrl; ?>#gradient-hoop')'><use xlink:href='#icon-hoop'/></svg>
-		    </div><div class='about-txt col-3'>
+		    </div><div class='about-img-wrapper'>
+		    	<div class='about-img about-img5'>
+		    		<img src='<?php echo get_template_directory_uri(); ?>/layoutImg/about5.jpg' alt=''>
+		    		<svg class='icon hoop' style='fill:url(<?php echo $currentUrl; ?>#gradient-hoop')'><use xlink:href='#icon-hoop'/></svg>
+		    	</div>
+		    	<div class='about-img about-img6'>
+		    		<img src='<?php echo get_template_directory_uri(); ?>/layoutImg/about6.jpg' alt=''>
+		    		<svg class='icon hoop' style='fill:url(<?php echo $currentUrl; ?>#gradient-hoop')'><use xlink:href='#icon-hoop'/></svg>
+		    	</div>
+		    </div><div class='about-txt about-txt3 col-3'>
 		    	<h2><?php the_field('title3'); ?></h2>
 		    	<?php the_field('txt3'); ?>
+		    </div>
+
+		    <div class='about-img about-img7'>
+		    	<img src='<?php echo get_template_directory_uri(); ?>/layoutImg/about7.jpg' alt=''>
+		    	<svg class='icon hoop' style='fill:url(<?php echo $currentUrl; ?>#gradient-hoop')'><use xlink:href='#icon-hoop-thin'/></svg>
+		    	<div class='mask'></div>
 		    </div>
 
 		<?php else : ?>
