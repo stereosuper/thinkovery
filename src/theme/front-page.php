2ceae2e49fccae2114831798d50b26d01830197a
adrienlementheour <adrien.le.mentheour@gmail.com>
2017-01-02 17:08:14 +0100

    :bug: Firefox video wrapper flex

--- src/theme/front-page.php
+++ src/theme/front-page.php
@@ -62,9 +62,11 @@ get_header(); ?>
                 <?php the_field('section1Txt1'); ?>
                 <?php if(get_field('section1Video1')){ ?>
                     <div class='wrapper-video'>
-                        <iframe src='<?php the_field('section1Video1'); ?>?enablejsapi=1&html5=1' frameborder='0' allowfullscreen></iframe>
-                        <div class='cover-video' style='background-image:url(<?php echo wp_get_attachment_url(get_field('section1Cover1')); ?>)'>
-                            <svg class='icon hoop' style='fill:url(<?php echo $currentUrl; ?>#gradient-hoop')'><use xlink:href='#icon-hoop-thin'/></svg>
+                        <div>
+                            <iframe src='<?php the_field('section1Video1'); ?>?enablejsapi=1&html5=1' frameborder='0' allowfullscreen></iframe>
+                            <div class='cover-video' style='background-image:url(<?php echo wp_get_attachment_url(get_field('section1Cover1')); ?>)'>
+                                <svg class='icon hoop' style='fill:url(<?php echo $currentUrl; ?>#gradient-hoop')'><use xlink:href='#icon-hoop-thin'/></svg>
+                            </div>
                         </div>
                     </div>
                 <?php } ?>
@@ -73,9 +75,11 @@ get_header(); ?>
                 <?php the_field('section1Txt2'); ?>
                 <?php if(get_field('section1Video2')){ ?>
                     <div class='wrapper-video'>
-                        <iframe src='<?php the_field('section1Video2'); ?>?enablejsapi=1&html5=1' frameborder='0' allowfullscreen></iframe>
-                        <div class='cover-video' style='background-image:url(<?php echo wp_get_attachment_url(get_field('section1Cover2')); ?>)'>
-                            <svg class='icon hoop' style='fill:url(<?php echo $currentUrl; ?>#gradient-hoop')'><use xlink:href='#icon-hoop-thin'/></svg>
+                        <div>
+                            <iframe src='<?php the_field('section1Video2'); ?>?enablejsapi=1&html5=1' frameborder='0' allowfullscreen></iframe>
+                            <div class='cover-video' style='background-image:url(<?php echo wp_get_attachment_url(get_field('section1Cover2')); ?>)'>
+                                <svg class='icon hoop' style='fill:url(<?php echo $currentUrl; ?>#gradient-hoop')'><use xlink:href='#icon-hoop-thin'/></svg>
+                            </div>
                         </div>
                     </div>
                 <?php } ?>
