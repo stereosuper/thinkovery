21c9206d91e21affa0d29a52c3fed98df2005700
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-01-05 16:35:26 +0100

    :art: intro page type

--- src/theme/page.php
+++ src/theme/page.php
@@ -5,6 +5,11 @@
 	        <?php if ( have_posts() ) : the_post(); ?>
 
 	            <h1><?php the_title(); ?></h1>
+
+	            <?php if(get_field('intro')){ ?>
+	                <div class='intro'><?php the_field('intro'); ?></div>
+	            <?php } ?>
+
 	            <?php the_content(); ?>
 
 	        <?php else : ?>
