ebefe0f02d0a16e48064b2bb42c99f5a55cb8707
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-01-06 15:32:05 +0100

    :bug: fix #24 tel iphone

--- src/theme/header.php
+++ src/theme/header.php
@@ -49,6 +49,7 @@
 	<head>
 		<meta charset='utf-8'>
 		<meta name='viewport' content='width=device-width,initial-scale=1'>
+		<meta name='format-detection' content='telephone=no'>
 
 		<link rel='alternate' type='application/rss+xml' title='<?php echo get_bloginfo('sitename'); ?> Feed' href='<?php echo get_bloginfo('rss2_url'); ?>'>
 
