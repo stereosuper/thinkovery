700813fbc17fb9c4a399289bcf7a9f66859e93a4
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-01-13 16:11:21 +0100

    :art: remove widget

--- src/theme/footer.php
+++ src/theme/footer.php
@@ -90,7 +90,6 @@
                         <div class='container-medium'>
                             <p><?php _e('Nous envoyons environ une newsletter par mois. Garanti sans spam.', 'thinkovery'); ?></p>
                             <?php echo do_shortcode('[mc4wp_form id="8558"]'); ?>
-                            <?php dynamic_sidebar( 'sidebar' ); ?>
                         </div>
                     </div>
                 </div>
