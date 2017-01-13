070da3623085037e6df444302938fb9ca6d3208a
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-01-13 17:02:48 +0100

    :art: 404 trad

--- src/theme/404.php
+++ src/theme/404.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 
-    <img src='<?php echo get_template_directory_uri(); ?>/layoutImg/404.jpg' alt="<?php _e("Erreur 404 - Trump n'a pas du regarder de MOOC..", 'thinkovery'); ?>">
+    <img src='<?php echo get_template_directory_uri(); ?>/layoutImg/404.jpg' alt="<?php _e("404 error - We wish Trump have watched MOOCs...", 'thinkovery'); ?>">
     <p class='h2'>
-        <?php _e("Lui, il n'a pas du regarder de", 'thinkovery'); ?> <span>M<span>O</span>OC</span>...
+        <?php _e("We wish he would have watched", 'thinkovery'); ?> <span>M<span>O</span>OCs</span>...
     </p>
 
 <?php get_footer(); ?>
