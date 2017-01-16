67ce3f4d0254bb3fbc852eb24bc034c34e2c3828
Boris Forconi <boris@stereosuper.fr>
2017-01-16 17:29:41 +0100

    Open/Close newsletter

--- src/js/newsletter.js
+++ src/js/newsletter.js
@@ -6,10 +6,14 @@ module.exports = function(){
     var btnNewsletter = $('#btnNewsletter'), newsletter = $('#newsletter');
 
     function openNewsletter(){
-        TweenMax.to(newsletter, 0.4, {height: 'auto', onComplete: function(){
-            TweenMax.to(newsletter, 0.4, {opacity: 1});
-            TweenMax.to(window, 1, {scrollTo:{y: $(document).outerHeight()}});
-        }});
+        if(newsletter.css('opacity') == 0) {
+            TweenMax.to(newsletter, 0.4, {height: 'auto', onComplete: function(){
+                TweenMax.to(newsletter, 0.4, {opacity: 1});
+                TweenMax.to(window, 1, {scrollTo:{y: $(document).outerHeight()}});
+            }});
+        } else {
+            TweenMax.to(newsletter, 0.4, {opacity: 0, height: '0px'});
+        }
     }
 
     btnNewsletter.on('click', function(e){
