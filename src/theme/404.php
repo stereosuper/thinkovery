09836fabdca6a9c87811779908f498bbe9be5fb4
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-01-18 14:46:57 +0100

    :art: 404 field

--- src/theme/404.php
+++ src/theme/404.php
@@ -2,7 +2,7 @@
 
     <img src='<?php echo get_template_directory_uri(); ?>/layoutImg/404.jpg' alt="<?php _e("404 error - We wish Trump have watched MOOCs...", 'thinkovery'); ?>">
     <p class='h2'>
-        <?php _e("We wish he would have watched", 'thinkovery'); ?> <span>M<span>O</span>OCs</span>...
+        <?php the_field('404', 'options'); ?>
     </p>
 
 <?php get_footer(); ?>
