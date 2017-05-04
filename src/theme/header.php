10dbac86c1a9a6714c5e76d57a12ead36b4b0481
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-05-04 17:22:12 +0200

    :art: replace google analytics width tag manager

--- src/theme/header.php
+++ src/theme/header.php
@@ -76,19 +76,23 @@
 		<script src='https://use.typekit.net/uhd8avk.js'></script>
 		<script>try{Typekit.load({ async: false });}catch(e){}</script>
 
+		<!-- Google Tag Manager -->
 		<script>
-		  	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		  	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		  	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		  	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-		  	ga('create', 'UA-90031922-1', 'auto');
-		  	ga('send', 'pageview');
+			(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+			})(window,document,'script','dataLayer','GTM-TKWFKWJ');
 		</script>
+		<!-- End Google Tag Manager -->
 	</head>
 
 	<body <?php body_class('theme-'.$declis[$currentDecli]['mainColor']); ?> data-theme='<?php echo $declis[$currentDecli]['mainColor']; ?>'>
 
+		<!-- Google Tag Manager (noscript) -->
+		<noscript><iframe src='https://www.googletagmanager.com/ns.html?id=GTM-TKWFKWJ' height='0' width='0' style='display:none'></iframe></noscript>
+		<!-- End Google Tag Manager (noscript) -->
+		
 		<header role='banner' id='header' <?php if(!$cookie && is_front_page()) echo "class='cookie-on'"; ?>>
 			<?php if(!$cookie && is_front_page()){ ?>
 				<div id='cookie' class='cookie'>
