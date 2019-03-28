fed5c367081ba9c158b8bdc9f091998488a86727
Elisabeth Hamel <elisabeth@stereosuper.fr>
2019-03-28 11:24:00 +0100

    :art: responsive title + blockquote

--- wp-content/themes/think/footer.php
+++ wp-content/themes/think/footer.php
@@ -9,5 +9,11 @@
         <script id='hs-script-loader' async defer src='//js.hs-scripts.com/4019924.js'></script>
         <!-- End of HubSpot Embed Code -->
 
+        <svg aria-hidden="true" style="position:absolute;width:0;height:0;overflow:hidden;" version="1.1" xmlns="http://www.w3.org/2000/svg">
+            <defs>
+            </defs>
+        </svg>
+
+
         </body>
     </html>
