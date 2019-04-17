87f775397c9a39949788a410581282e3951aba9d
Alphability <albanmezino@gmail.com>
2019-04-17 11:51:55 +0200

    🎨 references list mobile style
    Signed-off-by: Alphability <albanmezino@gmail.com>

--- wp-content/themes/think/references.php
+++ wp-content/themes/think/references.php
@@ -1,34 +1,60 @@
 <?php 
 /*
-Template Name: Reference
+Template Name: References
 */
 
-get_header(); ?>
+get_header();
+
+$references_index = 0;
+$references_query_args = array(
+    'post_type' => 'reference',
+    'posts_per_page' => -1
+);
+$references_query = new WP_Query($references_query_args);
+
+$quote = get_field('references_quote');
+$source_name = get_field('references_source_name');
+$source_position = get_field('references_source_position');
+$source_company = get_field('references_source_company');
+?>
 
 <div class='container clearfix'>
-
-	<?php if ( have_posts() ) : the_post(); ?>
-
+	<?php if (have_posts()): the_post(); ?>
         <h1><?php the_title(); ?></h1>
         <div class='col-2-desk'><?php the_content(); ?></div>
-        
-        <?php $refQuery = new WP_Query( array('post_type' => 'reference') ); ?>
-
-        <?php if ( $refQuery->have_posts() ) : ?>
-            <ul class='reference-list'>
-                <?php while ( $refQuery->have_posts() ) : $refQuery->the_post(); ?>
-                    <li class='reference-item'>
+        <?php if ( $references_query->have_posts() ): ?>
+            <ul class='references-list'>
+                <?php while ($references_query->have_posts()): $references_query->the_post(); ?>
+                    <li class='references-item'>
                         <a href='<?php the_permalink(); ?>' title='<?php the_title(); ?>'>
                             <?php echo wp_get_attachment_image(get_field('logo'), 'full'); ?>
                         </a>
                     </li>
-                <?php endwhile; ?>
+                    <?php if ($references_index === 7 && $quote): ?>
+                        <li class='references-quote'>
+                            <svg class="icon"><use href="#icon-quote"/></svg>
+                            <blockquote class="quote">
+                                <p><?php echo $quote ?></p>
+                            </blockquote>
+                            <?php if ($source_name): ?>
+                                <cite class="source-name"><?php echo $source_name ?></cite>
+                            <?php endif; ?>
+                            <?php if ($source_position): ?>
+                                <p class="source-position"><?php echo $source_position ?></p>
+                            <?php endif; ?>
+                            <?php if ($source_company): ?>
+                                <p class="source-company"><?php echo $source_company ?></p>
+                            <?php endif; ?>
+                        </li>
+                <?php
+                        endif;
+                    $references_index += 1;
+                    endwhile;
+                ?>
             </ul>
             <?php wp_reset_postdata(); ?>
         <?php endif; ?>
-	
 	<?php endif; ?>
-
 </div>
 
 <?php get_footer(); ?>
