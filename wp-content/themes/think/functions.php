a49bf3d202fa8f9dbd2951698962a27c8d983d01
Alphability <albanmezino@gmail.com>
2019-04-30 17:28:13 +0200

    🔥 ça dégage 💩
    Signed-off-by: Alphability <albanmezino@gmail.com>

--- wp-content/themes/think/functions.php
+++ wp-content/themes/think/functions.php
@@ -277,7 +277,7 @@ function think_register_required_plugins(){
             'slug'        => 'stereoberg',
             'source'     => get_template_directory_uri() . '/plugins/stereoberg.zip',
             'required'    => true,
-            'version' => '1.0.2',
+            'version' => '1.0.3',
             'force_activation' => false
         ),
         array(
