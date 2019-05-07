a549a190542d645da9f28dc9eed4ebeb73330bfd
Elisabeth Hamel <elisabeth@stereosuper.fr>
2019-05-07 18:29:51 +0200

    :art: start blog

--- wp-content/themes/think/index.php
+++ wp-content/themes/think/index.php
@@ -6,20 +6,11 @@
 
 	<?php wp_list_categories( array('title_li' => '') ); ?> 
 
-	<?php if ( have_posts() ) : 
-		/*global $paged;
-		if(get_query_var('paged')){
-			$paged = get_query_var('paged');
-		}elseif(get_query_var('page')){
-			$paged = get_query_var('page');
-		}else{
-			$paged = 1;
-		} */
-	?>
+	<?php if ( have_posts() ) : ?>
 
 		<?php while ( have_posts() ) : the_post(); ?>
 			
-			<article>
+			<div>
 
 				<span><?php echo get_the_date(); ?></span>
 				<h2><?php the_title(); ?></h2>
@@ -27,14 +18,14 @@
 				<span>
 					<?php if( get_the_category() ){
 						foreach( get_the_category() as $cat ){
-							echo $cat->cat_name . ' - ';
+							echo '<a href="' . get_category_link( $cat->term_id ) . '">' . $cat->cat_name . '</a>' . ' - ';
 						}
 					} ?>
 				</span>
 				<?php the_excerpt(); ?>
 				<a href='<?php the_permalink(); ?>'><?php _e('Read more'); ?></a>
 
-			</article>
+			</div>
 		
 		<?php endwhile; ?>
 
